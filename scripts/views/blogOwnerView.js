b5a94a7bc7d3ee445da499b4d5bef4272a70a107
nemalp <lazarov.pl@gmail.com>
2016-03-15 00:01:04 +0200

    Update blogOwnerView to support to trigger login event

--- scripts/views/blogOwnerView.js
+++ scripts/views/blogOwnerView.js
@@ -4,6 +4,15 @@ app.blogOwnerView = (function() {
     function showLoginPage(selector) {
         $.get('templates/login.html', function(template) {
             $(selector).html(template);
+
+            $('#login').on('click', function() {
+                var username = $('#username').val(),
+                    password = $('#password').val();
+
+                Sammy(function() {
+                    this.trigger('login', {username:username, password: password});
+                });
+            });
         });
     }
 
