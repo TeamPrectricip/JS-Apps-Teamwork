34a0c326e78423195cb42d91d42e0e0aa4128e37
nemalp <lazarov.pl@gmail.com>
2016-03-14 19:26:25 +0200

    Initialize blogOwner model and controller

--- scripts/app.js
+++ scripts/app.js
@@ -6,16 +6,20 @@ var app = app || {};
             var requester = Object.create(app.requester).init('kid_-y1QostQk-', 'owner', '1234');
 
             var homeView = app.homeView.load();
+            var blogOwnerView = app.blogOwnerView.load();
 
             var homeModel = Object.create(app.homeModel).init(requester);
+            var blogOwnerModel = Object.create(app.blogOwnerModel).init(requester);;
 
             var homeController = Object.create(app.homeController).init(homeModel, homeView);
+            var blogOwnerController = Object.create(app.blogOwnerController).init(blogOwnerModel, blogOwnerView);
 
             this.get('#/', function() {
                 homeController.loadAllPosts(selector);
             });
 
             this.get('#/login', function() {
+                blogOwnerController.login();
             });
         });
 
