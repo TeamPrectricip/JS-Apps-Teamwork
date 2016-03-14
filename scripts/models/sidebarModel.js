d5809399955b697249ed035a226081e744997fdb
Georgi Matsov <georgi.macov@gmail.com>
2016-03-14 20:44:58 +0200

    Add sidebarModel.js - partly ready

---
+++ scripts/models/sidebarModel.js
@@ -0,0 +1,17 @@
+var app = app || {};
+
+app.sidebarModel = (function () {
+    var sidebarModel = {
+        init: function (requester) {
+            this._requester = requester;
+            this.serviceUrl = requester.baseUrl + 'appdata/' + requester.appId + '/posts';
+            return this;
+        },
+
+        getAllPosts: function () {
+            return this._requester.get(this.serviceUrl, false);
+        }
+    }
+
+    return sidebarModel;
+}());
