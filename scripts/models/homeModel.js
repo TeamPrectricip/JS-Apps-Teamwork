65ce321b63e8275e9e8c58e0d07d616bef1874c1
nemalp <lazarov.pl@gmail.com>
2016-03-14 12:44:34 +0200

    Add Home Model

--- scripts/models/homeModel.js
+++ scripts/models/homeModel.js
@@ -1,1 +1,17 @@
+var app = app || {};
 
+app.homeModel = (function() {
+    var homeModel = {
+        init: function(requester) {
+            this._requester = requester;
+            this.serviceUrl = requester.baseUrl + 'appdata/' + requester.appId + '/posts';
+            return this;
+        },
+
+        getAllPosts: function() {
+            return this._requester.get(this.serviceUrl, false);
+        }
+    }
+
+    return homeModel;
+}());
