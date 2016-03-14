778ae30265469c277c736f83f608e968007e4613
nemalp <lazarov.pl@gmail.com>
2016-03-14 12:46:16 +0200

    Add homeController

---
+++ scripts/controllers/homeController.js
@@ -0,0 +1,34 @@
+var app = app || {};
+
+app.homeController = (function() {
+    var homeController = {
+        init: function(model, view) {
+            this._model = model;
+            this._view = view;
+            return this;
+        },
+
+        loadAllPosts: function(selector) {
+            var that = this;
+
+            this._model.getAllPosts()
+                .then(function(posts) {
+                    var data = {
+                        posts: []
+                    }
+
+                    posts.forEach(function(post) {
+                        data.posts.push({
+                            title: post.title,
+                            author: post.author,
+                            text: post.text
+                        });
+                    });
+
+                that._view.showHomePage(selector, data);
+                }).done();
+        }
+    }
+
+    return homeController;
+}());
