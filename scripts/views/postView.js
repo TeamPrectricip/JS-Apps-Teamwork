2645dd5bed39f7249e529e3800023da5dfdc09cf
Georgi Matsov <georgi.macov@gmail.com>
2016-03-15 22:08:39 +0200

    Add post view times counter

--- scripts/views/postView.js
+++ scripts/views/postView.js
@@ -8,7 +8,23 @@ app.postView = (function () {
             $.sammy(function () {
                 this.trigger('get-comments', {parent: $('.comments'), id: data.posts[0]._id})
             });
-            $('#addComment').on('click', function(){
+
+            function postViewCount(postId) {
+
+                var localstorageItem = 'totalCount-' + postId;
+                if (!localStorage.getItem(localstorageItem)) {
+
+                    localStorage.setItem(localstorageItem, 0);
+                }
+                var currentCount = parseInt(localStorage.getItem(localstorageItem));
+                currentCount++;
+                localStorage.setItem(localstorageItem, currentCount);
+
+                $('#visit-count').text('Visit for ' + currentCount + '  time');
+            }
+
+            postViewCount(data.posts[0]._id)
+            $('#addComment').on('click', function () {
                 var textArea = $('#textArea').val();
                 $.sammy(function () {
                     this.trigger('add-comment', {postId: data.posts[0]._id, content: textArea, parent: $('.comments')})
