bc77d052152e99e79ea4de3b829fef4bb0dffbc8
nemalp <lazarov.pl@gmail.com>
2016-03-17 01:00:29 +0200

    Add code to truncate post body

--- scripts/controllers/homeController.js
+++ scripts/controllers/homeController.js
@@ -14,17 +14,40 @@ app.homeController = (function() {
             this._model.getAllPosts()
                 .then(function(posts) {
                     var data = {
-                        posts: []
+                        posts: [],
+                        sortedPosts: []
                     };
 
                     posts.forEach(function(post) {
+                        if(post.text.length > 50) {
+                            var showText = post.text.substr(0, 40) + '...';
+                        }
+
                         data.posts.push({
+                            title: post.title,
+                            author: post.author,
+                            text: showText,
+                            id: post._id
+                        });
+                    });
+
+                    var sorted = posts.sort(function(a, b) {
+                        var firstDate = new Date(a._kmd.ect),
+                            secondDate = new Date(b._kmd.ect);
+
+                        return firstDate - secondDate;
+                    });
+
+                    sorted.forEach(function(post) {
+                        data.sortedPosts.push({
                             title: post.title,
                             author: post.author,
                             text: post.text,
                             id: post._id
+
                         });
                     });
+
 
                 that._view.showHomePage(selector, data);
                 }).done();
